99241e26b9d3a2e5e75302ff21f1741f01206ea0
Sapper <59349073+wushengzhu@users.noreply.github.com>
2023-11-27 17:44:04 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import theme from './theme.js'
 
 export default defineUserConfig({
-  base: '/interview-column/',
+  base: '/interview/',
 
   locales: {
     // "/": {
